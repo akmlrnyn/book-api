5581a6fc9474edb0929107fe5149364312d17732
akmlrnyn <akmalranyan@gmail.com>
2024-05-06 10:19:07 +0700

    connected to mysql driver

---
+++ config/database.go
@@ -0,0 +1,24 @@
+package config
+
+import (
+	"fmt"
+	"log"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func ConnectDB () {
+	connection := fmt.Sprintf("$v:$v@tcp($v:$v)/$v?pasreTime=true&loc=Asia$vJakarta", ENV.DB_USER, ENV.DB_PASSWORD, ENV.DB_HOST, ENV.DB_PORT, ENV.DB_DATABASE, "%2f")
+
+	db, err := gorm.Open(mysql.Open(connection), &gorm.Config{})
+
+	if err != nil {
+		log.Fatal("Database failed to connect")
+	}
+
+	DB = db
+	fmt.Println("Database Connected")
+}
